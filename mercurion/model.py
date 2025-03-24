4c1b9f097aed5dfbdfd97cd5b61a2e22f84b35b2
giuliomarinelli <giulio.marinelli@icloud.com>
2025-03-24 20:04:12 +0100

    def model

---
+++ mercurion/model.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+
+class MercurionMLP(nn.Module):
+    def __init__(self, input_dim=2048, hidden_dims=[512, 128], output_dim=12, dropout=0.3):
+        super(MercurionMLP, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, hidden_dims[0]),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+
+            nn.Linear(hidden_dims[0], hidden_dims[1]),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+
+            nn.Linear(hidden_dims[1], output_dim)
+            # ⚠️ Nessuna Sigmoid qui → usiamo BCEWithLogitsLoss! <=> BINARY CROSSENTROPY CON pos_weight
+        )
+
+    def forward(self, x):
+        return self.model(x)
