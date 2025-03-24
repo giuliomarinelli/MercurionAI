f7015cf1fcc61ade054bd89a54c358227cd68c37
giuliomarinelli <giulio.marinelli@icloud.com>
2025-03-24 19:31:41 +0100

    raw dataset

--- scripts/download_tox21.py
+++ scripts/download_tox21.py
@@ -2,6 +2,7 @@ import deepchem as dc
 import pandas as pd
 
 def download_tox21(output_path='data/raw/tox21.csv'):
+    print("Download dataset Tox21")
     tox21_tasks, datasets, _ = dc.molnet.load_tox21(featurizer='Raw', split='random')
     train_dataset, _, _ = datasets
 
@@ -9,6 +10,11 @@ def download_tox21(output_path='data/raw/tox21.csv'):
         'smiles': train_dataset.ids,
         'labels': list(train_dataset.y)
     })
+    
+    df = pd.DataFrame(train_dataset.y, columns=tox21_tasks)
+    df['smiles'] = train_dataset.ids
+    df.to_csv(output_path, index=False)
+
 
     df.to_csv(output_path, index=False)
     print(f"Tox21 salvato in: {output_path}")
